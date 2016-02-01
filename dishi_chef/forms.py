1403c6ff8c60c0facadaabd273fce63130d79a21
jobm <jobmwaniki18@gmail.com>
2016-02-01 17:35:34 +0300

    modified forms.py file and added some notes

--- dishi_chef/forms.py
+++ dishi_chef/forms.py
@@ -1,32 +1,46 @@
 from django import forms
-from .models import Chef,business_types,kitchen_types,Kitchen
+from .models import Chef, business_types, kitchen_types, Kitchen
+
+"""
+look into crispy-forms-foundation and model the forms around that, it also
+comes with validation called 'abide' look at that as well, it may prove abit
+challenging but incase of problems, tell me.
+"""
 
 
+# create a chef form here"""
+class ChefForm(forms.ModelForm):
+    # add chef fields here
+    pass
 
+
+# this form should only have about three fields for now
 class KitchenForm(forms.ModelForm):
-	Kitchen_choices=[('a','Start a Food Business'),('b','Scale an existing food business'),('c','Sell food in my spare time'),('d','Offer cooking classes'),]
-	
-	bussiness_type = forms.TypedChoiceField(choices=Kitchen_choices, widget=forms.RadioSelect)
 
-	kitchen_types=[('a','Bakery'),('b','African Cuisine'),('c','Intercontinental Cuisine'),]
+    class Meta:
+        model = Kitchen
+        fields = ['kitchen_name', 'bussiness_type', 'kitchen_type']
+    bussiness_type = forms.ChoiceField(
+        choices=Kitchen_choices, widget=forms.RadioSelect)
 
-	kitchen_type = forms.TypedChoiceField(choices=kitchen_types, widget=forms.RadioSelect)
+    kitchen_type = forms.ChoiceField(
+        choices=kitchen_types, widget=forms.SelectMultiple)
 
-	class Meta:
-		model=Kitchen
-		fields=['full_name','email','password','confirm_password','kitchen_type']
+    # The below method cleans data and allows for only .com extension emails
+    # to be used
+    # we wont need to do form validation for now but we will revisit it later
+    """
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if ".com" not in email:
+            raise forms.ValidationError(
+                "Please use your moringa email to register")
+        return email
 
-	#The below method cleans data and allows for only .com extension emails to be used
-
-	def clean_email(self):
-		email=self.cleaned_data.get('email')
-		if not ".com" in email:
-			raise forms.ValidationError("Please use your moringa email to register")
-		return email
-
-	def clean(self):
-		password = self.cleaned_data.get('password')
-		confirm_password = self.cleaned_data.get('confirm_password')
-		if password and password != confirm_password:
-			raise forms.ValidationError("Passwords don't match")
-		return self.cleaned_data
+    def clean(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if password and password != confirm_password:
+            raise forms.ValidationError("Passwords don't match")
+        return self.cleaned_data
+    """
