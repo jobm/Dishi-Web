60f661a2beaae2912b4de7cbf2192ad67a36bdf7
jobm <jobmwaniki18@gmail.com>
2016-02-06 12:21:28 +0300

    added mandrill backend

--- Dishi_Web/settings.py
+++ Dishi_Web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'djril',
     'crispy_forms',
     'crispy_forms_foundation',
     'registration',
