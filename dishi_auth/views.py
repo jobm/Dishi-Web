2b4f1ae0e01d116fc5877a3ec167c417e77ee83e
Victorgichohi <victorgichohi48@gmail.com>
2016-02-11 14:45:48 +0300

    added auth settings

--- dishi_auth/views.py
+++ dishi_auth/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 # Create your views here.
 def auth_home(request):
     return render(request, 'index.html')
+
+
+def home(request):
+   context = RequestContext(request,
+                           {'user': request.user})
+   return render_to_response('auth.html',
+                             context_instance=context)
